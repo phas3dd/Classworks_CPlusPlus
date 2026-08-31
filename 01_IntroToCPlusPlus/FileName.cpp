bea6a2a6b2d9116a13e0be44daf037850c21f6ae
CODER <CODER@A26-PC9>
2026-08-31 16:20:38 +0300

    Add project files.

---
+++ 01_IntroToCPlusPlus/FileName.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main() {
+	
+	cout << "I\n";
+	cout << "\tlove\n";
+	cout << "\t\tyou\n";
+	cout << "\t\t\tC++!\n\n";
+	cout << "\t...:::RESUME:::...\n\n";
+	cout << "Name:\t\t\t Vlad\n";
+	cout << "Surname:\t\t Polyukhovych\n";
+	cout << "Last name:\t\t Serhiivych\n\n";
+	cout << "Date of birth:\t\t 22.11.2011\n";
+	cout << "City:\t\t\t Rivne\n";
+	cout << "Age:\t\t\t 17\n\n";
+	cout << "Hobby:\t\t\t C++\n";
+	cout << "::................................:\n\n";
+	cout << "Every\n";
+	cout << "\thunter\n";
+	cout << "\t\twants\n";
+	cout << "\t\t\tto\n";
+	cout << "\t\t\t\tknow\n";
+	cout << "\t\t\t\t\twhere\n";
+	cout << "\t\t\t\t\t\tthe\n";
+	cout << "\t\t\t\t\t\t\tpheasant\n";
+	cout << "\t\t\t\t\t\t\t\tis.\n";
+	
+
+	return 0;
+}
