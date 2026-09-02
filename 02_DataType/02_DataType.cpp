0c701c8b95d06ce0f218e0a3bf745bc75562d4bd
phas3dd <vladoq13@gmail.com>
2026-09-02 16:24:22 +0300

    homework1

--- 02_DataType/02_DataType.cpp
+++ 02_DataType/02_DataType.cpp
@@ -21,4 +21,75 @@ int main() {
 	float k_30_in_grams = k_day * days;
 	float transform_kg = k_30_in_grams / 1000;
 	cout << "Purchase of feed for 30 days: " << transform_kg << " kg" << endl;
+
+	// zav 1
+
+	int num1;
+	int num2;
+	int num3;
+	cout << "Enter first number: ";
+	cin >> num1;
+	cout << "Enter second number: ";
+	cin >> num2;
+	cout << "Enter third number: ";
+	cin >> num3;
+	int sum = num1 + num2 + num3;
+	cout << "First number: " << num1 << "\nSecond number: " << num2 << "\nThird number: " << num3 << "\nSumma of this numbers: " << sum << endl;
+
+	// zav 2
+
+	float number1;
+	float number2;
+	cout << "Enter first number: ";
+	cin >> number1;
+	cout << "Enter second number: ";
+	cin >> number2;
+	
+	float arithmetic = (number1 + number2) / 2;
+
+	cout << "arithmetic mean of your 2 numbers: " << arithmetic << endl;
+
+	// zav 3
+
+	int numberkm;
+	cout << "Enter kilometers: ";
+	cin >> numberkm;
+
+	int meters = numberkm * 1000;
+	
+	cout << meters << " Meters in " << numberkm << " kilometers" << endl;
+
+	// zav 4
+
+	const int item_price_1 = 150;
+	const int item_price_2 = 200;
+	const int item_price_3 = 175;
+	const int item_price_4 = 140;
+
+	int item_quant_1;
+	int item_quant_2;
+	int item_quant_3;
+	int item_quant_4;
+
+	cout << "Enter the quantity of first item: ";
+	cin >> item_quant_1;
+	cout << "Enter the quantity of second item: ";
+	cin >> item_quant_2;
+	cout << "Enter the quantity of third item: ";
+	cin >> item_quant_3;
+	cout << "Enter the quantity of fourth item: ";
+	cin >> item_quant_4;
+
+	int item_1 = item_price_1 * item_quant_1;
+	int item_2 = item_price_2 * item_quant_2;
+	int item_3 = item_price_3 * item_quant_3;
+	int item_4 = item_price_4 * item_quant_4;
+
+	int summary = item_1 + item_2 + item_3 + item_4;
+
+	cout << "Item 1 | 150 | x" << item_quant_1 << endl;
+	cout << "Item 2 | 200 | x" << item_quant_2 << endl;
+	cout << "Item 3 | 175 | x" << item_quant_3 << endl;
+	cout << "Item 4 | 140 | x" << item_quant_4 << endl;
+	cout << "Summary | " << summary << endl;
 }
---
+++ 03_Operators/03_Operators.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a = 5, b = 4;
+	cout << a + b << endl;
+	cout << a - b << endl;
+	cout << a * b << endl;
+	cout << a / b << endl;
+	cout << a % b << endl;
+	cout << 3 % 7 << endl;
+	cout << 8 % 9 << endl;
+	cout << 10 % 9 << endl;
+
+	int a = 2;
+	cout << "a = " << a << endl;
+	a = a + 1;
+	cout << "a = " << a << endl;
+	a++;
+	cout << "a = " << a << endl;
+	++a;
+	cout << "a = " << a << endl;
+}
